93ad9e0e7e7b1a72b2ca54ce0364b1c4db9f09c5
Leo <barrazaluciano2023@gmail.com>
2025-10-24 01:11:20 -0300

    Agregar archivos de proyecto.

---
+++ frmPrincipal.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace pryBarrazaBarMilangaaa
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dtvDatos = new DataGridView();
+            btnValidar = new Button();
+            btnMozoDia = new Button();
+            btnTotales = new Button();
+            ((System.ComponentModel.ISupportInitialize)dtvDatos).BeginInit();
+            SuspendLayout();
+            // 
+            // dtvDatos
+            // 
+            dtvDatos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dtvDatos.Location = new Point(95, 24);
+            dtvDatos.Name = "dtvDatos";
+            dtvDatos.Size = new Size(578, 309);
+            dtvDatos.TabIndex = 0;
+            // 
+            // btnValidar
+            // 
+            btnValidar.Location = new Point(679, 171);
+            btnValidar.Name = "btnValidar";
+            btnValidar.Size = new Size(111, 46);
+            btnValidar.TabIndex = 1;
+            btnValidar.Text = "Validar Datos";
+            btnValidar.UseVisualStyleBackColor = true;
+            btnValidar.Click += btnValidar_Click;
+            // 
+            // btnMozoDia
+            // 
+            btnMozoDia.Location = new Point(95, 359);
+            btnMozoDia.Name = "btnMozoDia";
+            btnMozoDia.Size = new Size(129, 40);
+            btnMozoDia.TabIndex = 2;
+            btnMozoDia.Text = "Mozo del Dia";
+            btnMozoDia.UseVisualStyleBackColor = true;
+            // 
+            // btnTotales
+            // 
+            btnTotales.Location = new Point(95, 405);
+            btnTotales.Name = "btnTotales";
+            btnTotales.Size = new Size(129, 40);
+            btnTotales.TabIndex = 3;
+            btnTotales.Text = "Totales";
+            btnTotales.UseVisualStyleBackColor = true;
+            // 
+            // frmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnTotales);
+            Controls.Add(btnMozoDia);
+            Controls.Add(btnValidar);
+            Controls.Add(dtvDatos);
+            Name = "frmPrincipal";
+            Text = "frmPrincipal";
+            Load += frmPrincipal_Load;
+            ((System.ComponentModel.ISupportInitialize)dtvDatos).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dtvDatos;
+        private Button btnValidar;
+        private Button btnMozoDia;
+        private Button btnTotales;
+    }
+}
---
+++ frmPrincipal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryBarrazaBarMilangaaa
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            int indiFila = 0;
+            int indiColumna = 0;
+
+            dtvDatos.Columns.Add("colMozos", "mozos");
+            dtvDatos.Columns.Add("colComidas", "Comidas");
+            dtvDatos.Columns.Add("colBebidasSinAlchol", "Bebidas Sin Alchol");
+            dtvDatos.Columns.Add("colBebidasConAlchol", "Bebidas Con Alchol");
+            dtvDatos.Columns.Add("colPostres", "Postres");
+
+            dtvDatos.Rows.Add("Julio", "", "", "", "");
+            dtvDatos.Rows.Add("Esteban", "", "", "", "");
+            dtvDatos.Rows.Add("Javier", "", "", "", "");
+            dtvDatos.Rows.Add("Gonzalo", "", "", "", "");
+            dtvDatos.Rows.Add("Alberto", "", "", "", "");
+
+            for (indiFila = 0; indiFila < dtvDatos.Rows.Count - 1; indiFila++)
+            {
+                for (indiColumna = 1; indiColumna < dtvDatos.Columns.Count; indiColumna++)
+                {
+                    dtvDatos[indiColumna, indiFila].Value = 0;
+                }
+            }
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            //dtvDatos[0,1].Style.BackColor = Color.Red;
+        }
+
+        
+    }
+}
